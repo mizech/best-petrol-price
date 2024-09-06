3390543a5fd6bc4bc92f6917f0cb6a272a22cc4f
mizech <michael.zech@hotmail.de>
2024-09-06 15:45:34 +0200

    Read API_KEY from Bundle

--- BestPetrolPrice/ViewModels/ContentViewModel.swift
+++ BestPetrolPrice/ViewModels/ContentViewModel.swift
@@ -5,15 +5,21 @@ class ContentViewModel {
     var stations = [Station]()
     
     func fetchData(radius: String = "10", type: String = "all") async {
-        let urlStr = "https://creativecommons.tankerkoenig.de/json/list.php?lat=52.521&lng=13.438&rad=\(radius)&sort=dist&type=\(type)&apikey=00000000-0000-0000-0000-000000000002"
-        if let url = URL(string: urlStr) {
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                let response = try JSONDecoder().decode(Response.self, from: data)
-                self.stations = response.stations
-            } catch {
-                print(error)
+        let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+        
+        if let apiKey = apiKey {
+            let urlStr = "https://creativecommons.tankerkoenig.de/json/list.php?lat=52.521&lng=13.438&rad=\(radius)&sort=dist&type=\(type)&apikey=\(apiKey)"
+            if let url = URL(string: urlStr) {
+                do {
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    let response = try JSONDecoder().decode(Response.self, from: data)
+                    self.stations = response.stations
+                } catch {
+                    print(error)
+                }
             }
+        } else {
+            print("Something has gone wrong, reading the API-key.")
         }
     }
 }
