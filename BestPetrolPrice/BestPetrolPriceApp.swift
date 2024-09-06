6e7c2d0ad77fdc7141dc1122f36ba47d64cdfd6d
mizech <michael.zech@hotmail.de>
2024-09-06 10:24:22 +0200

    Initial Commit

---
+++ BestPetrolPrice/BestPetrolPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestPetrolPriceApp.swift
+//  BestPetrolPrice
+//
+//  Created by Michael on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct BestPetrolPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
